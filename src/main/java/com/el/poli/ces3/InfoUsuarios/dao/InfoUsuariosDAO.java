f8654804e99c0aa3dfcb41e45e141cba682ff36b
Rediat <yaidercc@gmail.com>
2022-07-28 21:37:46 -0500

    metodos getInfoUsuario y getUsuarios definidos en el DAO

--- src/main/java/com/el/poli/ces3/InfoUsuarios/dao/InfoUsuariosDAO.java
+++ src/main/java/com/el/poli/ces3/InfoUsuarios/dao/InfoUsuariosDAO.java
@@ -5,7 +5,8 @@ import com.el.poli.ces3.InfoUsuarios.model.InfoUsuarios;
 import java.util.List;
 
 public interface InfoUsuariosDAO {
-
+    List<InfoUsuarios> getUsuarios();
+    InfoUsuarios getInfoUsuario(int id);
     InfoUsuarios createInfoUsuario(InfoUsuarios info_usuario);
     InfoUsuarios updateInfoUsuario(int id, InfoUsuarios info_usuario);
 
